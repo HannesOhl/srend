cef065ad9188c08f7d1fc7b0e860ed102dadc221
hanni <ohldag.hannes@gmail.com>
2026-02-27 17:08:15 +0100

    added lighting (simple, diffuse)

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ typedef struct {
 	uint32_t flags;
 	bool grid_on;
 	bool wireframe;
+	bool bfc;
 } State;
 
 typedef struct {
@@ -40,7 +41,8 @@ typedef struct {
 State state = {
 	.flags = 0,
 	.grid_on = true,
-	.wireframe = false
+	.wireframe = false,
+	.bfc = false
 };
 
 void pixel_set(uint32_t x, uint32_t y, uint32_t* buffer, uint32_t color) {
@@ -188,8 +190,6 @@ void line_draw(V3f p1, V3f p2, uint32_t* buffer, uint32_t color, Camera camera) 
 	}
 }
 
-
-
 void grid_draw(uint32_t* buffer, Camera camera) {
 
 	int32_t grid_const = 40;
@@ -219,99 +219,126 @@ void background_render(uint32_t* buffer) {
 		buffer[x + y*SCREEN_WIDTH] = 0;
 	}}
 }
+static inline Color shade_color(Color c, float intensity)
+{
+    if (intensity < 0.0f) intensity = 0.0f;
+    if (intensity > 1.0f) intensity = 1.0f;
 
+    uint32_t i = (uint32_t)(intensity * 255.0f);
 
-bool is_point_in_triangle(V2s p, V2s v1, V2s v2, V2s v3) {
+    uint32_t r = ((c >> 24) & 0xFF) * i >> 8;
+    uint32_t g = ((c >> 16) & 0xFF) * i >> 8;
+    uint32_t b = ((c >>  8) & 0xFF) * i >> 8;
+    uint32_t a =  (c        & 0xFF);
 
-	float denominator = (v2.y - v3.y)*(v1.x - v3.x) + (v3.x - v2.x)*(v1.y - v3.y);
-	if (fabsf(denominator) < EPS) return false;
-
-	float a = ((v2.y - v3.y)*(p.x - v3.x) + (v3.x - v2.x)*(p.y - v3.y)) / denominator;
-	float b = ((v3.y - v1.y)*(p.x - v3.x) + (v1.x - v3.x)*(p.y - v3.y)) / denominator;
-	float c = 1.0f - a - b;
-
-	return a >= 0.0f && b >= 0.0f && c >= 0.0f && a <= 1.0f && b <= 1.0f && c <= 1.0f;
+    return (r << 24) | (g << 16) | (b << 8) | a;
 }
 
 void triangle_draw(Triangle t, uint32_t* buffer, Camera camera, Color color) {
-
 
 	if (state.wireframe) {
 		line_draw(t.v1, t.v2, buffer, color, camera);
 		line_draw(t.v1, t.v3, buffer, color, camera);
 		line_draw(t.v2, t.v3, buffer, color, camera);
-	} else {
-		/*
-		line_draw(t.v1, t.v2, buffer, color, camera);
-		line_draw(t.v1, t.v3, buffer, color, camera);
-		line_draw(t.v2, t.v3, buffer, color, camera);
-		*/
-		// change to cam basis
-		t.v1 = world_to_view(t.v1, camera);
-		t.v2 = world_to_view(t.v2, camera);
-		t.v3 = world_to_view(t.v3, camera);
-
-		float znear = camera.znear;
-		if (t.v1.z <= znear && t.v2.z <= znear && t.v3.z <= znear) return;
-
-		V2s v1 = get_image_crd(t.v1, camera);
-		V2s v2 = get_image_crd(t.v2, camera);
-		V2s v3 = get_image_crd(t.v3, camera);
-
-		//buffer_depth[??]
-
-		// skip trangles that are completely out of the image
-		if ( v1.x < (int32_t) XMIN && v2.x < (int32_t) XMIN && v3.x < (int32_t) XMIN) return;
-		if ( v1.y < (int32_t) YMIN && v2.y < (int32_t) YMIN && v3.y < (int32_t) YMIN) return;
-		if ( v1.x > (int32_t) XMAX && v2.x > (int32_t) XMAX && v3.x > (int32_t) XMAX) return;
-		if ( v1.y > (int32_t) YMAX && v2.y > (int32_t) YMAX && v3.y > (int32_t) YMAX) return;
-
-		int32_t x_min = v1.x;
-		if (v2.x < x_min) x_min = v2.x;
-		if (v3.x < x_min) x_min = v3.x;
-		int32_t x_max = v1.x;
-		if (v2.x > x_max) x_max = v2.x;
-		if (v3.x > x_max) x_max = v3.x;
-		int32_t y_min = v1.y;
-		if (v2.y < y_min) y_min = v2.y;
-		if (v3.y < y_min) y_min = v3.y;
-		int32_t y_max = v1.y;
-		if (v2.y > y_max) y_max = v2.y;
-		if (v3.y > y_max) y_max = v3.y;
-
-		// adjust bounding box to the images clipping space
-		if (x_min < (int32_t) XMIN) x_min = (int32_t) XMIN;
-		if (x_max > (int32_t) XMAX) x_max = (int32_t) XMAX;
-		if (y_min < (int32_t) YMIN) y_min = (int32_t) YMIN;
-		if (y_max > (int32_t) YMAX) y_max = (int32_t) YMAX;
-
-		for(int32_t x = x_min; x <= x_max; x++) {
-		for(int32_t y = y_min; y <= y_max; y++) {
-			V2s p = (V2s) { x, y };
-			if (is_point_in_triangle(p, v1, v2, v3)) {
-
-				float denominator = (v2.y - v3.y)*(v1.x - v3.x)
-					          + (v3.x - v2.x)*(v1.y - v3.y);
-				if (fabsf(denominator) < EPS) return;
-
-				float a = ( (v2.y - v3.y) * (x   - v3.x)
-					  + (v3.x - v2.x) * (p.y - v3.y) ) / denominator;
-				float c = 1.0f - a;
-				float z_inv = 1.0f / t.v1.z * c
-					    + 1.0f / t.v2.z * a;
-				float z = 1.0f / z_inv;
-
-				size_t idx = x + y * (size_t) SCREEN_WIDTH;
-				if (z < buffer_depth[idx]) {
-					buffer_depth[idx] = z;
-					pixel_set(x, y, buffer, color);
-				}
-			}
-		}}
-		triangle_count_global += 1;
+		return;
 	}
-}
 
+	// change to cam basis (view space)
+	t.v1 = world_to_view(t.v1, camera);
+	t.v2 = world_to_view(t.v2, camera);
+	t.v3 = world_to_view(t.v3, camera);
+
+	float znear = camera.znear;
+	if (t.v1.z <= znear && t.v2.z <= znear && t.v3.z <= znear) return;
+
+	// also for lighting
+	V3f n = cross_3f( sub_3f(t.v2, t.v1), sub_3f(t.v3, t.v1) );
+	if (state.bfc) {
+		// cull backfaces
+		if ( dot_3f(t.v1, n) <= 0 ) return;
+	}
+
+	V2s v1 = get_image_crd(t.v1, camera);
+	V2s v2 = get_image_crd(t.v2, camera);
+	V2s v3 = get_image_crd(t.v3, camera);
+
+	// skip trangles that are completely out of the image
+	if ( v1.x < (int32_t) XMIN && v2.x < (int32_t) XMIN && v3.x < (int32_t) XMIN) return;
+	if ( v1.y < (int32_t) YMIN && v2.y < (int32_t) YMIN && v3.y < (int32_t) YMIN) return;
+	if ( v1.x > (int32_t) XMAX && v2.x > (int32_t) XMAX && v3.x > (int32_t) XMAX) return;
+	if ( v1.y > (int32_t) YMAX && v2.y > (int32_t) YMAX && v3.y > (int32_t) YMAX) return;
+
+	// bounding box
+	int32_t x_min = v1.x;
+	if (v2.x < x_min) x_min = v2.x;
+	if (v3.x < x_min) x_min = v3.x;
+	int32_t x_max = v1.x;
+	if (v2.x > x_max) x_max = v2.x;
+	if (v3.x > x_max) x_max = v3.x;
+	int32_t y_min = v1.y;
+	if (v2.y < y_min) y_min = v2.y;
+	if (v3.y < y_min) y_min = v3.y;
+	int32_t y_max = v1.y;
+	if (v2.y > y_max) y_max = v2.y;
+	if (v3.y > y_max) y_max = v3.y;
+
+	// adjust bounding box to the images clipping space
+	if (x_min < (int32_t) XMIN) x_min = (int32_t) XMIN;
+	if (x_max > (int32_t) XMAX) x_max = (int32_t) XMAX;
+	if (y_min < (int32_t) YMIN) y_min = (int32_t) YMIN;
+	if (y_max > (int32_t) YMAX) y_max = (int32_t) YMAX;
+
+	// precompute floats for screen coords
+	float v1x = (float)v1.x, v1y = (float)v1.y;
+	float v2x = (float)v2.x, v2y = (float)v2.y;
+	float v3x = (float)v3.x, v3y = (float)v3.y;
+	float iz1 = 1.0f / t.v1.z;
+	float iz2 = 1.0f / t.v2.z;
+	float iz3 = 1.0f / t.v3.z;
+
+	float denom = (v2y - v3y)*(v1x - v3x) + (v3x - v2x)*(v1y - v3y);
+	if (fabsf(denom) < EPS) return;
+
+	// get shaded color for triangle based on global light pos
+	n = norm_3f(n);
+	V3f light_global_pos_w = { .x = 5.0f, .y = 5.0f, .z = 5.0f };
+	V3f light_global_pos_v = world_to_view(light_global_pos_w, camera);
+	V3f dir = norm_3f(sub_3f(light_global_pos_v, t.v1));
+	float intensity = dot_3f(dir, n);
+	if (intensity < 0.0f) intensity = 0.0f;
+	color = shade_color(color, intensity);
+
+	float invDenom = 1.0f / denom;
+
+	for (int32_t y = y_min; y <= y_max; ++y) {
+	for (int32_t x = x_min; x <= x_max; ++x) {
+
+		// sample at pixel center
+		float px = (float) x + 0.5f;
+		float py = (float) y + 0.5f;
+
+		// barycentric coords
+		float a = ( (v2y - v3y) * (px - v3x) + (v3x - v2x) * (py - v3y) ) * invDenom;
+		float b = ( (v3y - v1y) * (px - v3x) + (v1x - v3x) * (py - v3y) ) * invDenom;
+		float c = 1.0f - a - b;
+
+		// TODO: this is a hack, we need a consistent top-left rule
+		const float BC_EPS = 1e-6f;
+		if (a < -BC_EPS || b < -BC_EPS || c < -BC_EPS) continue;
+
+		// get z for depth buffer
+		float z_inv = a * iz1 + b * iz2 + c * iz3;
+		if (fabsf(z_inv) < EPS) continue;
+		float z = 1.0f / z_inv;
+
+		size_t idx = (size_t) x + (size_t) y * (size_t) SCREEN_WIDTH;
+		if (z < buffer_depth[idx]) {
+			buffer_depth[idx] = z;
+			pixel_set(x, y, buffer, color);
+		}
+	}}
+    	triangle_count_global += 1;
+}
 
 void time_measure_start(struct timespec* t0) {
 	clock_gettime(CLOCK_MONOTONIC, t0);
@@ -365,6 +392,7 @@ void event_loop(SDLContext* ctx, uint32_t* buffer, Camera camera) {
 				if (ctx->event.key.keysym.sym == SDLK_ESCAPE) running = false;
 				if (ctx->event.key.keysym.sym == SDLK_g) state.grid_on = !state.grid_on;
 				if (ctx->event.key.keysym.sym == SDLK_w) state.wireframe = !state.wireframe;
+				if (ctx->event.key.keysym.sym == SDLK_b) state.bfc = !state.bfc;
 				break;
 
 			case SDL_MOUSEMOTION:
@@ -428,7 +456,7 @@ void event_loop(SDLContext* ctx, uint32_t* buffer, Camera camera) {
 		}
 */
 		static float angle = 0.0f;
-		//angle += 0.01;
+		angle += 0.01;
 		M3f mat_rot1 = rot_xz_3f(+angle);
 		M3f mat_rot2 = rot_xz_3f(-angle);
 
@@ -440,7 +468,7 @@ void event_loop(SDLContext* ctx, uint32_t* buffer, Camera camera) {
 			};
 			triangle_draw(t, buffer, camera, EMERALD);
 		}
-		/*
+
 		for (int i = asset_teapot.f_count - 1; i >= 0; i--) {
 
 			V3f v1 = {
@@ -468,7 +496,7 @@ void event_loop(SDLContext* ctx, uint32_t* buffer, Camera camera) {
 			};
 			triangle_draw(t, buffer, camera, ROUGE);
 		}
-		*/
+
 		//triangle_draw(tri1, buffer, camera, EMERALD);
 		//triangle_draw(tri2, buffer, camera, ROUGE);
 		//V3f origin = {{8.0f, 0.0f, 8.0f}};
@@ -490,10 +518,14 @@ void event_loop(SDLContext* ctx, uint32_t* buffer, Camera camera) {
 			samples_cur = 0;
 		}
 
-		text_render(string_format("frame time = %.2f ms, FPS = %.2f (256 samples);"
-					"lines drawn = %zuk triangles drawn = %zu\n",
+		text_render(string_format("frame time = %.2f ms, FPS = %.2f (256 samples), "
+					  "lines drawn = %zu, triangles drawn = %zu\n",
 					t_ms_avg, 1000.0 / t_ms_avg, lines_count_global,
 					triangle_count_global), 0, 0, buffer, GREEN, 2);
+
+		text_render(string_format("wireframe = %s\n", state.wireframe ? "on" : "off"), 0, 20, buffer, GREEN, 2);
+		text_render(string_format("backface culling = %s\n", state.bfc ? "on" : "off"), 0, 40, buffer, GREEN, 2);
+
 		lines_count_global     = 0;
 		triangle_count_global = 0;
 	}
